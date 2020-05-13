6abb68d3dd88509d83e2fdb98a7386e22adf4997
mlgmontage <kettleofrassel1@gmail.com>
2020-05-13 11:06:24 -0700

    new format

--- main.c
+++ main.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-   char thread[30];
+  char thread[30];
 
-   for (int i = 0; i < 30; i++)
-   {
-      thread[i] = 'a';
-   }
+  for (int i = 0; i < 30; i++)
+  {
+    thread[i] = 'a';
+  }
 
-   for (int i = 0; i < 30; i++)
-   {
-      printf("%p -> %c\n", thread + i, *(thread + i));
-   }
+  for (int i = 0; i < 30; i++)
+  {
+    printf("%p -> %c\n", thread + i, *(thread + i));
+  }
 
-   return 0;
+  return 0;
 }
