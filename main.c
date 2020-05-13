52ec91396f9f73040be15a9032cee9f07370bc9e
mlgmontage <kettleofrassel1@gmail.com>
2020-05-13 11:11:17 -0700

    fahr celcius converter

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
+  int fahr, celsius;
+  int lower, upper, step;
 
-   printf("Hello, world\n");
+  lower = 0;
+  upper = 300;
+  step = 20;
 
-   return 0;
+  fahr = lower;
+  while (fahr <= upper)
+  {
+    celsius = 5 * (fahr - 32) / 9;
+    printf("%d\t%d\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+
+  return 0;
 }
